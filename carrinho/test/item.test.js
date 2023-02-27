b1f367c04af989f3c2bb1f6673ec71f530304272
Douglas Leal <douglas.leal@live.com>
2023-02-27 20:27:38 -0300

    novos testes e conhecendo novos matchers

---
+++ carrinho/test/item.test.js
@@ -0,0 +1,26 @@
+import { describe } from "@jest/globals";
+import Item from "../item.js";
+
+describe("Testes dos itens", () => {
+    it("Deve ter 3 campos: nome, valor e quantidade", () => {
+        const item = new Item("Beterraba", 2.5, 10);
+
+        expect(item.nome).toBe("Beterraba");
+        expect(item.valor).toBe(2.5);
+        expect(item.quantidade).toBe(10);
+    })
+
+    it("Deve ter o preço calculado de acordo com a quantidade", () => {
+        const item = new Item("Beterraba", 2.5, 10);
+
+        expect(item.pegaValorTotalItem()).toBe(25);
+        
+    })
+
+    it("Deve ter o preço calculado de acordo com a quantidade", () => {
+        const item = new Item("Beterraba", 0.1, 3);
+
+        expect(item.pegaValorTotalItem()).toBeCloseTo(0.3);
+        
+    })
+});
